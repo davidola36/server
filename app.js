d29a866a7e5c61c2969d3b6c2be1bbf167286da6
david adeoye <davidola36@yahoo.com>
2017-08-08 11:08:10 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+var mongoose = require('mongoose')
+var fs = require('fs');
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost:/sample', function(err, db){
+	if (err) throw err
+	else console.log('it connected')
+	})
+	// creating a new model
+	var personSchema = mongoose.Schema({
+   fname: String,
+   lname: String,
+   email: String,
+   num: Number,
+   votes: { type: Number, default: 0 },
+   img: { data: Buffer, contentType: String }
+});
+//using personSchema to create person collection
+var Person = mongoose.model("Person", personSchema);
+
+//setting template engine to html
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true}));
+// for parsing multipart/form-data
+app.use(upload.array());
+//setting the root directory as views to watch
+app.use(express.static('views'));
+app.get('/', function(req, res){
+	res.render('photosplash');
+		
+});
+app.post('/submit.html', upload.single('profimage'), function(req, res){
+	//get parsed information
+	var personInfo = req.body;
+	if(!personInfo.lname || !personInfo.fname || !personInfo.num){
+      console.log("all the info has not been filled");}
+	   else {
+      var newPerson = new Person({
+         fname: personInfo.fname,
+         lname: personInfo.lname,
+		 email: personInfo.email,
+         num: personInfo.num
+      });
+		if(req.file.data){
+			cosole.log("the image has been referenced")
+			}else{
+					console.log("the image has not yet been referenced")
+				}
+      newPerson.save(function(err, Person){
+         if(err)
+            console.log("the file didnt save");
+         else
+           console.log("the file saved");
+		   console.log(req.body);
+      });
+   }
+});
+app.listen(3000);
---
+++ views/myscript.js
@@ -0,0 +1,113 @@
+
+
+//form validation
+var oForm = document.getElementById("myForm");
+	var oShow = document.getElementById("show")
+function validateForm(){
+	for (var i = 0; i < oForm.elements.length; i++){
+		if(oForm.elements[i].type == "text" && oForm.elements[i].value == ""){
+			oShow.innerHTML = "please input all relevant information";
+			return false;
+		}
+			
+	}
+	saveForm();
+}
+//get 64bit code from picture
+ function previewFile() {
+      var preview = document.getElementById("image");
+      var file    = document.querySelector('input[type=file]').files[0];
+      var reader  = new FileReader();
+    
+      reader.addEventListener("load", function () {
+        preview.src = reader.result;
+        document.getElementById("write").innerHTML = reader.result;
+      }, false);
+    
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+//initializing the array outside the functon
+var entry =[];
+//condition to retrieve data from memory
+if (localStorage.getItem("Json")){
+		entry = JSON.parse(localStorage.getItem("Json"))
+	} 
+	console.log(entry);
+	//saving form into the memory
+function saveForm(){
+	
+	var oJson = {
+			  "fname" :"name1",
+			  "lname" : "name2",
+			  "email" : "vemail",
+			  "pnumber" : "number",
+			  "vote" : "0",
+			  "date":"o",
+			  "pimage":"o"
+			}
+			// initializing the json object key values
+	oJson.fname = document.getElementById("fname").value;
+	oJson.lname = document.getElementById("lname").value;
+	oJson.email = document.getElementById("email").value;
+	oJson.pnumber = document.getElementById("num").value;
+	oJson.date = Date.parse(Date());
+	//oJson.pimage = document.getElementById("write").innerHTML;
+	entry.push(oJson);
+	console.log(oJson);
+	var sJson = JSON.stringify(entry);		
+	localStorage.setItem("Json", sJson);
+}
+
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope){
+	$scope.Json = JSON.parse(localStorage.getItem("Json"));
+	// adding votes
+	$scope.addVote = function (index){
+		console.log(index);
+		$scope.vote = parseInt($scope.Json[index].vote);
+		console.log($scope.vote);
+		$scope.vote++;
+		$scope.Json[index].vote = $scope.vote
+		localStorage.setItem("Json", JSON.stringify($scope.Json));
+				
+	}
+	$scope.Json.sort(function(a,b){return b.vote -a.vote});
+
+});
+app.controller("myCtrl2", function($scope){
+	$scope.Json = JSON.parse(localStorage.getItem("Json"));
+	// adding votes
+	$scope.addVote = function (index){
+		console.log(index);
+		$scope.vote = parseInt($scope.Json[index].vote);
+		console.log($scope.vote);
+		$scope.vote++;
+		$scope.Json[index].vote = $scope.vote
+		localStorage.setItem("Json", JSON.stringify($scope.Json));
+				
+	}
+	$scope.Json.sort(function(a,b){return b.date -a.date});
+
+});
+
+
+//slide show 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < slides.length;   i++) {
+        slides[i].className = slides[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
