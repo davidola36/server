dbacf737aed1e07cf618762be597dd826f444439
david adeoye <davidola36@yahoo.com>
2017-08-10 14:32:43 +0100

    testing 2

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.use(express.static('views'));
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+	console.log("it works");
 });
 /*app.get('/', function(req, res){
 	res.render('photosplash');
