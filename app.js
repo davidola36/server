4317cd9f1b91f4c7c4555ebd262115c1961a3492
david adeoye <davidola36@yahoo.com>
2017-08-11 14:20:11 +0100

    db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://davidola36:1cancomea@ds125262.mlab.com:25262/heroku_fhgfqzv4';
-//connect to mongodb
+////connect to mongodb
  // Makes connection asynchronously.  Mongoose will queue up database
     // operations and release them when the connection is complete.
     mongoose.connect(uristring, function (err, res) {
