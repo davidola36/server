9a6eef239861d0040975373fd22410387db01bc1
david adeoye <davidola36@yahoo.com>
2017-08-31 11:26:15 +0100

    multer

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var upload = multer({ dest: './uploads' });
 var mongoose = require('mongoose')
 var fs = require('fs');
 var uristring =
@@ -35,7 +34,12 @@ var Person = mongoose.model("Person", personSchema);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-
+//defining the path of the image
+app.use(multer({ dest: './uploads/',
+	rename: function (fieldname, filename){
+		return filename;
+		},
+	}));
 
 // for parsing application/json
 app.use(bodyParser.json());
@@ -50,7 +54,7 @@ app.get('/', function(req, res){
 	res.render('photosplash');
 		
 });
-app.post('/submit.html', upload.single('profimage'), function(req, res){
+app.post('/submit.html', function(req, res){
 	//get parsed information
 	var personInfo = req.body;
 	if(!personInfo.lname || !personInfo.fname || !personInfo.num){
@@ -60,8 +64,10 @@ app.post('/submit.html', upload.single('profimage'), function(req, res){
          fname: personInfo.fname,
          lname: personInfo.lname,
 		 email: personInfo.email,
-         num: personInfo.num
+         num: personInfo.num,
       });
+	  newPerson.img.data = fs.readFileSync(req.files.userPhoto.path)
+	  newPerson.img.contentType= 'image/png';
       newPerson.save(function(err, Person){
          if(err)
             console.log("the file didnt save");
