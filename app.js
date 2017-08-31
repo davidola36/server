18268c1c7f85dccc0abfb2d963ff2837bc35c40f
Your Name <you@example.com>
2017-08-31 13:00:58 +0200

    update

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer({ dest: './uploads' });
-var mongoose = require('mongoose')
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  routes = require('./controllers/index.js');
+ var multer = require('multer');
+ var upload = multer({ dest: './uploads' });
+// var 
 var fs = require('fs');
 var uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://davidola36:1cancomea@ds061374.mlab.com:61374/sample';
-////connect to mongodb
- // Makes connection asynchronously.  Mongoose will queue up database
-    // operations and release them when the connection is complete.
-    mongoose.connect(uristring, function (err, res) {
-      if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      } else {
-      console.log ('Succeeded connected to: ' + uristring);
-      }
-    });
-	// creating a new model
-	var personSchema = mongoose.Schema({
-   fname: String,
-   lname: String,
-   email: String,
-   num: Number,
-   votes: { type: Number, default: 0 },
-   img: { data: Buffer, contentType: String }
-});
-//using personSchema to create person collection
-var Person = mongoose.model("Person", personSchema);
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://davidola36:1cancomea@ds061374.mlab.com:61374/sample';
 
-//setting template engine to html
+
+////connect to mongodb
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connected to: ' + uristring);
+  }
+});
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -41,34 +33,16 @@ app.set('view engine', 'html');
 app.use(bodyParser.json());
 
 // for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // for parsing multipart/form-data
 app.use(upload.array());
 //setting the root directory as views to watch
 app.use(express.static('views'));
-app.get('/', function(req, res){
-	res.render('photosplash');
-		
-});
-app.post('/submit.html', upload.single('profimage'), function(req, res){
-	//get parsed information
-	var personInfo = req.body;
-	if(!personInfo.lname || !personInfo.fname || !personInfo.num){
-      console.log("all the info has not been filled");}
-	   else {
-      var newPerson = new Person({
-         fname: personInfo.fname,
-         lname: personInfo.lname,
-		 email: personInfo.email,
-         num: personInfo.num
-      });
-      newPerson.save(function(err, Person){
-         if(err)
-            console.log("the file didnt save");
-         else
-           console.log("the file saved");
-		   console.log(req.body);
-      });
-   }
-});
+
+
+app.use('/', routes);
+
+
+
 app.listen(process.env.PORT || 5000);
+console.log('app listening on port 5000')
